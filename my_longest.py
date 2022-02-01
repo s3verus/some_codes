b6be373515d2a77e3ff36e4726727ca12470c684
Ali Abdollahi <en.severus@gmail.com>
2022-02-01 12:04:45 +0330

    solving logical error in outer loop, imposter len

--- my_longest.py
+++ my_longest.py
@@ -3,10 +3,14 @@ inpu = input("input: ")
 lists = inpu.split(", ")[::-1]
 maxim = max(lists, key=len)
 
-for item in lists:
+lens = len(lists)
+for j in range(lens):
     for i in maxim:
         if i not in "ertabdfyh":
             lists.remove(maxim)
+            if len(lists) == 0:
+                maxim = ""
+                break
             maxim = max(lists, key=len) 
             break
 print(maxim)
