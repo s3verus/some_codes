8c6a03cc091b4838ef0617760e68a44a3f256d7f
Ali Abdollahi <en.severus@gmail.com>
2022-02-02 12:50:48 +0330

    solved logical error, like my_longest.py

--- my_longest_check_time.py
+++ my_longest_check_time.py
@@ -8,10 +8,14 @@ cate, cat, bat, bate, catayon
 lists = inpu.split(", ")[::-1]
 maxim = max(lists, key=len)
 
-for item in lists:
+lens = len(lists)
+for j in range(lens):
     for i in maxim:
         if i not in "ertabdfyh":
             lists.remove(maxim)
+            if len(lists) == 0:
+                maxim = ""
+                break
             maxim = max(lists, key=len) 
             break
 # print(maxim)
