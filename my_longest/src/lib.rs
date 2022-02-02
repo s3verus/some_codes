6c0292135c01f51ad050232b9a3895e32d22b39d
Ali Abdollahi <en.severus@gmail.com>
2022-02-02 11:20:19 +0330

    create mod work_with_vec, write some test, should handle some bugs

--- my_longest/src/lib.rs
+++ my_longest/src/lib.rs
@@ -1,11 +1,63 @@
-pub fn maxim<'a>(input_vec: &'a Vec<&str>) -> &'a str {
-    let mut maxim = "";
-    let mut maxim_len = 0;
-    for item in input_vec {
-        if maxim_len <= item.chars().count() {
-            maxim_len = item.chars().count();
-            maxim = item;
+mod work_with_vec {
+    pub mod iter_items {
+//    let maxim = input_vec.iter().max();
+//    let maxim = match maxim {
+//        Some(max) => max,
+//        None      => "Vector is empty",
+//    };
+        pub fn maxim<'a>(input_vec: &'a Vec<&str>) -> &'a str {
+            let mut maxim = "";
+            let mut maxim_len = 0;
+            for item in input_vec {
+                if maxim_len <= item.chars().count() {
+                    maxim_len = item.chars().count();
+                    maxim = item;
+                }
+            }
+            maxim
         }
     }
-    maxim
+}
+
+use crate::work_with_vec::iter_items;
+
+pub fn longest_valid_item(mut input_vec: Vec<&str>) -> &str {
+    let mut maxim = iter_items::maxim(&input_vec); 
+    let bad_guys = String::from("cgijklmnopqsuvwxz");
+    let mut vec_index = 0;
+    let mut iner_count = 0;
+    let len = input_vec.len();
+
+    while vec_index < len {
+        while iner_count < maxim.chars().count() {
+            if bad_guys.contains(&maxim[iner_count..iner_count+1]) {
+                // find index of maxim and remove
+                let index = input_vec.iter().position(|x| *x == maxim).unwrap();
+                input_vec.remove(index);
+                maxim = iter_items::maxim(&input_vec);
+                break;
+            }
+            iner_count += 1;
+        }
+        vec_index += 1;
+    }
+    println!("{}", maxim);
+    "i will handle it"
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn same_len() {
+        let input_vec = vec!["bat", "tab", "tad"];
+        assert_eq!("tad", longest_valid_item(input_vec));
+    }
+
+    #[test]
+    fn have_one() {
+        let input_vec = vec!["bat"];
+        assert_eq!("bat", longest_valid_item(input_vec));
+    }
 }
